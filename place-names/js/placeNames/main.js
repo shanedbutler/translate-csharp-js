d3eb502b4a5c3aff429a0fd3aeef835e4c916ad3
shanedbutler <shanedbutler@gmail.com>
2022-09-13 11:22:41 -0400

    Translate place-names cs to js

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,1 +1,26 @@
-// Put your code here
+// Array of names
+const names = ["Nashville", "Hong Kong", "The back yard", "Earth", "London", "The mall", "Ryman Auditorium", "The Great Wall of China"]
+
+// Print some output to the terminal (aka. "Console");
+console.log("All Place Name");
+
+// Iterate over each "name" in the "names" list
+for (const name of names) {
+
+    console.log(name);
+}
+
+// When you don't pass an argument to console.log, it simply outputs a blank line.
+//  Here we use that to add a little space around the output.
+console.log();
+
+// Filter the list to create a new list that only has 
+//  place names that start with the word "The".
+const theNames = names.filter(name => name.startsWith("The"));
+
+console.log("'The' Place Names");
+
+for (const name of theNames) {
+
+    console.log(name);
+}
