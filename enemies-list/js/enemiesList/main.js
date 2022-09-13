36536402529218fe93c312b93b9bb98bf6c2abff
shanedbutler <shanedbutler@gmail.com>
2022-09-13 14:21:38 -0400

    Translate enemiesList cs to js

--- enemies-list/js/enemiesList/main.js
+++ enemies-list/js/enemiesList/main.js
@@ -1,1 +1,51 @@
 // Put your code here
+console.log("My Enemies List!");
+console.log("----------------");
+
+const enemies = [
+    {
+        firstName: "Joshua",
+        lastName: "Flowers",
+        offenses: [
+            "Being a jerk to me in elementary school",
+            "Not being nice to me in elementary school"
+        ],
+        isReallyHated: true
+    },
+    {
+        firstName: "Darth",
+        lastName: "Flowers",
+        offenses: [
+            "Cut off Luke's hand",
+            "Not being nice to me in elementary school"
+        ],
+        isReallyHated: false
+    },
+    {
+        firstName: "Betty",
+        lastName: "Flowers",
+        offenses: [
+            "Phone calls in the theater",
+            "Not being nice to me in elementary school"
+        ],
+        isReallyHated: true
+    },
+    {
+        firstName: "Leon",
+        lastName: "Flowers",
+        offenses: [
+            "Keeps giving me a hotplate",
+            "Not being nice to me in elementary school"
+        ],
+        isReallyHated: false
+    },
+];
+
+for (const myEnemy of enemies) {
+    if (myEnemy.isReallyHated) {
+        console.log(`${myEnemy.firstName} ${myEnemy.lastName} (Really, really dislike!)`);
+    }
+    else {
+        console.log(`${myEnemy.firstName} ${myEnemy.lastName}`);
+    }
+}
