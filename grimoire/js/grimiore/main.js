b5f45e0e2f4d4dd3ec170ab0aa43a15d31692991
shanedbutler <shanedbutler@gmail.com>
2022-09-14 09:47:33 -0400

    Translate grimiore cs to js

--- grimoire/js/grimiore/main.js
+++ grimoire/js/grimiore/main.js
@@ -1,1 +1,71 @@
-// Put your code here
+const getAllSpells = () => {
+    
+    const allSpells = [
+        {
+            name: "Turn enemy into a newt",
+            isEvil: true,
+            energyRequired: 5.1
+        },
+        {
+            name: "Conjure some gold for a local charity",
+            isEvil: false,
+            energyRequired: 2.99
+        },
+        {
+            name: "Give a deaf person the ability to heal",
+            isEvil: false,
+            energyRequired: 12.2
+        },
+        {
+            name: "Make yourself emperor of the universe",
+            isEvil: true,
+            energyRequired: 100.0
+        },
+        {
+            name: "Convince your relatives your political views are correct",
+            isEvil: false,
+            energyRequired: 2921.5
+        }
+    ];
+    
+    return allSpells;
+}
+
+const makeEvilSpellBook = (allSpells) => {
+
+    const goodBook = {
+        title: "Good Book",
+        spells: allSpells.filter(spell => !spell.isEvil)
+    };
+
+    return goodBook;
+}
+
+const makeGoodSpellBook = (allSpells) => {
+
+    const evilBook = {
+        title: "Evil Book",
+        spells: allSpells.filter(spell => spell.isEvil)
+    };
+
+    return evilBook;
+}
+
+const displaySpellBook = (book) => {
+
+    console.log(book.title);
+    for (const spell of book.spells) {
+        
+        console.log(`   ${spell.name}`);
+    };
+}
+console.log("Do you believe in magic?");
+console.log("------------------------");
+
+const allSpells = getAllSpells();
+const goodBook = makeGoodSpellBook(allSpells);
+const evilBook = makeEvilSpellBook(allSpells);
+
+displaySpellBook(evilBook);
+console.log();
+displaySpellBook(goodBook);
